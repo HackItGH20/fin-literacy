e04bbde051941cc23260dd0792927ef224fa0522
lkobetz <larissa.kobetz@gmail.com>
2020-03-01 14:06:03 -0500

    added single article view

--- client/components/articles/Headline.js
+++ client/components/articles/Headline.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
-export const Headline = props => {
-  return (
-    <div>
-      <h3>{props.article.title}</h3>
-      <h3>{props.article.author}</h3>
-      <h3>{props.article.source.name}</h3>
-    </div>
-  )
+class Headline extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      redirect: false
+    }
+  }
+  changeView() {
+    this.setState({redirect: true})
+  }
+  render() {
+    return (
+      <div>
+        {this.state.redirect && (
+          <Redirect
+            to={{
+              pathname: `/articles/${this.props.article.id}`,
+              state: {article: this.props.article}
+            }}
+          />
+        )}
+        <h3 onClick={() => this.changeView()}>{this.props.article.title}</h3>
+        <h3>{this.props.article.author}</h3>
+        <h3>{this.props.article.source.name}</h3>
+      </div>
+    )
+  }
 }
 
 export default Headline
